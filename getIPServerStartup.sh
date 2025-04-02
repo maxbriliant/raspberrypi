60f630288f28fad8481a662a6a8563fd0e7d33ac
Maks Briliant <47421740+maxbriliant@users.noreply.github.com>
2025-04-02 19:49:05 +0200

    Add files via upload

---
+++ getIPServerStartup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "by MxBit2020 - RasberryRun19"
+echo "                 "
+echo "-------Good Morning------"
+echo "Your IP this morning is: "
+echo "       "$(dig +short myip.opendns.com @resolver1.opendns.com)
+echo "                 "
+echo "                 "
+
+
+
+
